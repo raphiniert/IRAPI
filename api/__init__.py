76135f143f7f2c5d01760ce5a00419b2354c3772
raphiniert <dev@raphiniert.eu>
2023-05-14 19:35:16 +0200

    added logging, handle startup and shutdown event, include devices router

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,21 @@
+import logging
+
 from fastapi import FastAPI, APIRouter
+from rich.logging import RichHandler
+
+from api.core.config import settings
+from api.database import async_engine
+from api.db.models import Base
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="DEBUG",
+    format=FORMAT,
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[RichHandler()],
+)
+
+logger = logging.getLogger("irserver")
 
 
 def create_app() -> FastAPI:
@@ -8,13 +25,26 @@ def create_app() -> FastAPI:
         version="0.1.0",
     )
 
+    @app.on_event("startup")
+    async def startup():
+        logger.info("startup")
+        async with async_engine.begin() as conn:
+            # await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+
+    @app.on_event("shutdown")
+    async def shutdown():
+        logger.info("shutdown")
+        await async_engine.dispose()
+
     router = APIRouter(
-        prefix="/api",
+        prefix=settings.router_prefix,
     )
 
-    from api.routers import signals
+    from api.routers import devices, signals
 
-    router.include_router(signals.router, prefix="/v1")
+    router.include_router(devices.router, prefix=settings.router_version_prefix)
+    router.include_router(signals.router, prefix=settings.router_version_prefix)
     app.include_router(router)
 
     return app
