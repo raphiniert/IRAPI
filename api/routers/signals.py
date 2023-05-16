948e432f7a8a1cf82a19204e2c672e74c9e4f0fc
raphiniert <dev@raphiniert.eu>
2023-05-16 21:24:31 +0200

    implemented get (list), create, patch and delete signal routes

--- api/routers/signals.py
+++ api/routers/signals.py
@@ -1,15 +1,62 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
+
+from api import schemas
+from api.crud.signals import (
+    create_signal,
+    delete_signal,
+    get_signal,
+    get_signals,
+    update_signal,
+)
+from api.database import get_db
 
 router = APIRouter(
     prefix="/signals",
     tags=["signals"],
     responses={
         400: {"description": "Bad request"},
+        401: {"description": "Unauthorized"},
+        403: {"description": "Forbidden"},
         404: {"description": "Not found"},
+        405: {"description": "Method not Allowed"},
     },
 )
 
 
-@router.get("/")
-async def read_signals() -> dict:
-    return {"Hello": "World"}
+@router.get("/", response_model=List[schemas.IRSignal])
+async def read_signals(db: AsyncSession = Depends(get_db)) -> List:
+    signals = await get_signals(db)
+    return signals
+
+
+@router.get("/{signal_id}", response_model=schemas.IRSignal)
+async def read_signal(signal_id: int, db: AsyncSession = Depends(get_db)):
+    db_signal = await get_signal(db=db, signal_id=signal_id)
+    if db_signal is None:
+        raise HTTPException(status_code=404, detail="IRSignal not found")
+    return db_signal
+
+
+@router.post("/", response_model=schemas.IRSignal, status_code=201)
+async def create_new_signal(
+    signal: schemas.IRSignalCreate, db: AsyncSession = Depends(get_db)
+):
+    return await create_signal(db=db, signal=signal)
+
+
+@router.patch("/{signal_id}", response_model=schemas.IRSignal)
+async def patch_signal(
+    signal_id: int, signal: schemas.IRSignal, db: AsyncSession = Depends(get_db)
+):
+    if signal_id != signal.id:
+        raise HTTPException(status_code=400, detail="IRSignal id's not matching")
+    signal = await get_signal(db=db, signal_id=signal_id)
+    return await update_signal(db=db, signal=signal)
+
+
+@router.delete("/{signal_id}", response_model=schemas.IRSignalDelete)
+async def remove_signal(signal_id: int, db: AsyncSession = Depends(get_db)):
+    db_signal = await delete_signal(db=db, signal_id=signal_id)
+    return {"message": f"IRSignal with id: {db_signal.id} deleted"}
