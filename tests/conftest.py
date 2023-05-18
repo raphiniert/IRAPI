cf9dd6a98d919a2f3ef2a0f25a5a1537187b0e11
raphiniert <dev@raphiniert.eu>
2023-05-18 12:18:48 +0200

    setup conftest

---
+++ tests/conftest.py
@@ -0,0 +1,44 @@
+import asyncio
+import pytest
+
+from fastapi import FastAPI
+from httpx import AsyncClient
+from typing import AsyncGenerator, Generator
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Generator:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="module")
+async def app() -> FastAPI:
+    from api import create_app
+    from api.database import async_engine
+    from api.db.models import Base
+
+    app = create_app()
+
+    # drop all tables and create new ones
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield app
+
+    # clean up afterwards
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="module")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture(scope="module")
+async def async_client(app: FastAPI) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
