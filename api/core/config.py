f02602a52f5a32129ee8a5756d6a3bebaf25a4a0
raphiniert <dev@raphiniert.eu>
2023-05-19 22:37:22 +0200

    added authentication settings

--- api/core/config.py
+++ api/core/config.py
@@ -6,12 +6,17 @@ class Settings(BaseSettings):
     router_prefix: str = "/api"
     router_version_prefix: str = "/v1"
 
-    # Database
+    # database
     db_server: str = Field(..., env="POSTGRES_SERVER")
     db_user: str = Field(..., env="POSTGRES_USER")
     db_password: SecretStr = Field(..., env="POSTGRES_PASSWORD")
     db_db: str = Field(..., env="POSTGRES_DB")
     db_port: str = Field(..., env="POSTGRES_PORT")
+
+    # authentication
+    private_key_pem: SecretStr = Field(..., env="PASETO_PRIVATE_KEY")
+    public_key_pem: SecretStr = Field(..., env="PASETO_PUBLIC_KEY")
+    access_token_expire_seconds: int = Field(..., env="ACCESS_TOKEN_EXPIRE_SECONDS")
 
     class Config:
         env_file = ".env"
