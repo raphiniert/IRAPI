093ad5f3dd699e56b0df57548982f03deb455aa1
raphiniert <dev@raphiniert.eu>
2023-05-22 20:41:33 +0200

    user async sessionmaker instead of sessionmaker, refactor get_db to auto rollback on failure

--- api/database.py
+++ api/database.py
@@ -1,9 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+import logging
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 from sqlalchemy.pool import NullPool
 
 from api.core.config import settings
 
+logger = logging.getLogger("irapi")
 
 DATABASE_URL = (
     f"postgresql+asyncpg://{settings.db_user}:{settings.db_password.get_secret_value()}"
@@ -13,12 +15,17 @@ DATABASE_URL = (
 # https://docs.sqlalchemy.org/en/20/orm/extensions/asyncio.html#using-multiple-asyncio-event-loops
 async_engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
 
-AsyncSessionLocal = sessionmaker(
-    async_engine, class_=AsyncSession, expire_on_commit=False
-)
+AsyncSessionLocal = async_sessionmaker(async_engine, expire_on_commit=False)
 
 
 async def get_db() -> AsyncSession:
     async with AsyncSessionLocal() as db:
-        yield db
-        await db.commit()
+        try:
+            yield db
+            # await db.commit()
+        except:
+            logger.warning("DB operation failed, trying to auto-rollback.")
+            await db.rollback()
+            raise
+        finally:
+            await db.close()
